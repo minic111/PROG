0496b381177c36e6483b02a1f7db9f2d8643c907
minic111 <115929542+minic111@users.noreply.github.com>
2022-10-17 09:09:51 +0200

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String args[]) {
+
+    System.out.print("Hallo")
+    System.out.println(args[0]);
+    System.out.println(" und ");
+    System.out.println(args[1]);
+    
+    
+}
+}
